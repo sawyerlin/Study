a91e62a5237300af15dc39de07c984d58483fe20
sawyerlin <sawyer.lin@gmail.com>
2015-02-10 17:04:51 +0100

    add character driver

--- linux/driver01/ofd.c
+++ linux/driver01/ofd.c
@@ -1,14 +1,29 @@
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+
+static dev_t first;
 
 static int __init ofd_init(void) {
+
     printk(KERN_INFO "Namaskar: ofd registered\n");
+
+    if (alloc_chrdev_region(&first, 0, 3, "Shweta") < 0)
+    {
+        return -1;
+    }
+
+    printk(KERN_INFO "<Major, Minor>: <%d, %d>\n", MAJOR(first), MINOR(first));
+
     return 0;
 }
 
 static void __exit ofd_exit(void) {
     printk(KERN_INFO "Alvida: ofd unregistered\n");
+    unregister_chrdev_region(first, 3);
 }
 
 module_init(ofd_init);
